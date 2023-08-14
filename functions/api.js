e789e1bf237233358c9ea7ce147222d2f36c25b3
BaHGet <abosama150@gmail.com>
2023-08-15 00:44:30 +0300

    smail reorgins in api.js

--- functions/api.js
+++ functions/api.js
@@ -6,11 +6,6 @@ const app = express();
 
 const mongoose = require("mongoose");
 const cors = require("cors");
-app.use(
-  cors({
-    origin: "*",
-  })
-);
 
 mongoose.connect('mongodb+srv://abosama150:MSg6fl6TrYkUBc9e@hany-server.px9xs90.mongodb.net/account');
 const db = mongoose.connection;
@@ -18,6 +13,13 @@ db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
 
 const accountsRouter = require("../routes/accounts");
 const route = express.Router()
